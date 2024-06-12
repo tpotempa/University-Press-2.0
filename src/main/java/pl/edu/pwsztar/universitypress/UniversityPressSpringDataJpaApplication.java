eeef422b23d4dde961298efea153fec4b71e383e
tpotempa <tpotempa@gmail.com>
2024-06-12 14:34:06 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pwsztar/universitypress/UniversityPressSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwsztar.universitypress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityPressSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityPressSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/edu/pwsztar/universitypress/model/PaperBook.java
@@ -0,0 +1,75 @@
+package pl.edu.pwsztar.universitypress.model;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import static javax.persistence.FetchType.EAGER;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
+
+@Entity
+@NamedEntityGraph(name = "Book.authors",
+        attributeNodes = @NamedAttributeNode("authors")
+)
+@NamedEntityGraph(name = "Book.authors-categories",
+        attributeNodes = {
+                @NamedAttributeNode("authors"),
+                @NamedAttributeNode("categories")
+        }
+)
+
+public class PaperBook extends Book {
+
+    private String binding;
+
+    @ManyToMany(fetch = EAGER)
+    @Fetch(FetchMode.JOIN)
+    private List<Author> authors = new ArrayList<>();
+
+    @ManyToMany
+    private Set<Category> categories = new LinkedHashSet<>();
+
+    public PaperBook() {
+    }
+
+    public PaperBook(String isbn, String title, String description, LocalDate publishingDate, String binding) {
+        super(isbn, title, description, publishingDate);
+        this.binding = binding;
+    }
+
+    public String getBinding() {
+        return binding;
+    }
+
+    public void setBinding(String binding) {
+        this.binding = binding;
+    }
+
+    @Override
+    public List<Author> getAuthors() {
+        return null;
+    }
+
+    @Override
+    public void setAuthors(List<Author> authors) {
+
+    }
+
+    @Override
+    public Set<Category> getCategories() {
+        return null;
+    }
+
+    @Override
+    public void setCategories(Set<Category> categories) {
+
+    }
+}
